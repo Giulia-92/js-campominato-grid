970f6d81ccdea67206c6614cf85108f57eb31b89
Giulia-92 <cionco.giulia@gmail.com>
2022-04-04 19:13:18 +0200

    js-campominato-grid

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.getElementById("play").addEventListener("click", function(){
-    play();
-}
+//document.getElementById("play").addEventListener("click", function(){
+    //play()
 
 
 
@@ -15,19 +14,41 @@ document.getElementById("play").addEventListener("click", function(){
 
 
 
-function play(){
+
+/*function play(){
 console.log("play");
 document.getElementById("livelli").value;
 console.log("livelli",.value)
-}
+};*/
 
+
+const griglia = document.getElementById("container");
+const contenuto = document.createElement("div");
+contenuto.setAttribute("class", "row ");
+griglia.append(contenuto);
+for (let numero = 1; numero <= 49; numero++) {
+    const colonna = document.createElement("div");
+    colonna.setAttribute("class", "col-1");
+    const riga = document.createElement("div");
+    let Testo;
+}
 
 
 
 for (let numero = 1; numero <= 100; numero++) {
     const colonna = document.createElement("div");
-    colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
-    const colonna2 = document.createElement("div");
+    colonna.setAttribute("class", "col-1");
+    const riga = document.createElement("div");
+    let Testo;
+}
+
+
+
+for (let numero = 1; numero <= 81; numero++) {
+    const colonna = document.createElement("div");
+    colonna.setAttribute("class", "col-1");
+    const riga = document.createElement("div");
+    let Testo;
 }
 
 
@@ -35,12 +56,3 @@ for (let numero = 1; numero <= 100; numero++) {
 
 
 
-/*const cesto = document.getElementById("cesto");
-const linea = document.createElement("div");
-linea.setAttribute("class", "row g-3 mt-4");
-cesto.append(linea);
-for (let numero = 1; numero <= 100; numero++) {
-    const colonna = document.createElement("div");
-    colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
-    const colonna2 = document.createElement("div");
-    let Testo;*/
