eded4055707aeaf7cf6c2da9f7b8257c7b21e1f6
Giulia-92 <cionco.giulia@gmail.com>
2022-04-04 18:40:05 +0200

    js-campominato-grid

---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById("play").addEventListener("click", function(){
+    play();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function play(){
+console.log("play");
+document.getElementById("livelli").value;
+console.log("livelli",.value)
+}
+
+
+
+
+for (let numero = 1; numero <= 100; numero++) {
+    const colonna = document.createElement("div");
+    colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
+    const colonna2 = document.createElement("div");
+}
+
+
+
+
+
+
+/*const cesto = document.getElementById("cesto");
+const linea = document.createElement("div");
+linea.setAttribute("class", "row g-3 mt-4");
+cesto.append(linea);
+for (let numero = 1; numero <= 100; numero++) {
+    const colonna = document.createElement("div");
+    colonna.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
+    const colonna2 = document.createElement("div");
+    let Testo;*/
